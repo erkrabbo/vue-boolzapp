2bf057f36389270c3fb4f65d8b28e30c0f4e7c10
Luca Andaloro <luca.andaloro4@gmail.com>
2022-03-15 17:25:23 +0100

    lista chat dinamica

---
+++ script/script.js
@@ -0,0 +1,96 @@
+
+const app = new Vue({
+    el: '#vue-app',
+    data:{
+        chatArchive: [
+            {
+                name: "Michele",
+                profileImg: 'avatar_1.jpg',
+                chatContent:[
+                    {
+                        mine: false,
+                        text: 'ciao',
+                        time: '15.30',
+                    }
+                ],
+            },
+            {
+                name: "Fabio",
+                profileImg: 'avatar_2.jpg',
+                chatContent:[
+                    {
+                        mine: false,
+                        text: 'ciao',
+                        time: '15.30',
+                    }
+                ],
+            },
+            {
+                name: "Samuele",
+                profileImg: 'avatar_3.jpg',
+                chatContent:[
+                    {
+                        mine: false,
+                        text: 'ciao',
+                        time: '15.30',
+                    }
+                ],
+            },
+            {
+                name: "Alessandro B.",
+                profileImg: 'avatar_4.jpg',
+                chatContent:[
+                    {
+                        mine: false,
+                        text: 'ciao',
+                        time: '15.30',
+                    }
+                ],
+            },
+            {
+                name: "Alessandro L.",
+                profileImg: 'avatar_5.jpg',
+                chatContent:[
+                    {
+                        mine: false,
+                        text: 'ciao',
+                        time: '15.30',
+                    }
+                ],
+            },
+            {
+                name: "Claudia",
+                profileImg: 'avatar_6.jpg',
+                chatContent:[
+                    {
+                        mine: false,
+                        text: 'ciao',
+                        time: '15.30',
+                    }
+                ],
+            },
+            {
+                name: "Federico",
+                profileImg: 'avatar_7.jpg',
+                chatContent:[
+                    {
+                        mine: false,
+                        text: 'ciao',
+                        time: '15.30',
+                    }
+                ],
+            },
+            {
+                name: "Davide",
+                profileImg: 'avatar_8.jpg',
+                chatContent:[
+                    {
+                        mine: false,
+                        text: 'ciao',
+                        time: '15.30',
+                    }
+                ],
+            },
+        ]
+    }
+})
